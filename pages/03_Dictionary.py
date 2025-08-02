b97a17540d2d5545abcc7208a1918ba8d0f8b626
Lin Francis <64030176+LinFrancis@users.noreply.github.com>
2025-08-01 20:06:57 -0400

    Add files via upload

--- pages/03_Dictionary.py
+++ pages/03_Dictionary.py
@@ -7,7 +7,7 @@ import uuid
 st.set_page_config(
     page_title="林高天 Journey",
     layout="wide",
-    initial_sidebar_state="expanded"  # 👈 this keeps the sidebar open
+    initial_sidebar_state="expanded",  # 👈 this keeps the sidebar open
     page_icon="🌱"
 )
 
